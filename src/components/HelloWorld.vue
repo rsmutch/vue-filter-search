477ec368e7a6aa802e9ebcf36d309be6aab27854
Richard <rsmutch@gmail.com>
2021-03-23 20:15:32 +0000

    Adds search results component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,32 @@
 <template>
   <div>
-    <h2>Filter LearnVue Articles</h2>
+    <h2>Filter Articles</h2>
     <input type="text" placeholder="Filter Search" v-model="query" />
+    <br />
     {{ query }}
+    <br />
+    <button @click="reset">Reset</button>
+    <search-results :query="query" />
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
+import SearchResults from './SearchResults.vue';
 export default {
+  components: {
+    SearchResults
+  },
   setup() {
     const query = ref('');
 
+    const reset = (evt) => {
+      query.value = ''; // clears the query
+    };
+
     return {
-      query
+      query,
+      reset
     };
   }
 };
---
+++ src/components/SearchResults.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="root">
+    <p>Showing {{ filteredTitles.length }} results for "{{ query }}"</p>
+    <ul>
+      <li v-for="title in filteredTitles" :key="title.Page">
+        {{ title.Name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import titles from '../post-data.json';
+export default {
+  props: {
+    query: String
+  },
+  setup(props, context) {
+    const filteredTitles = computed(() => {
+      return titles.filter((s) =>
+        s.Name.toLowerCase().includes(props.query.toLowerCase())
+      );
+    });
+
+    return {
+      filteredTitles
+    };
+  }
+};
+</script>
